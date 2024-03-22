e6a59e1e01d108ad0d79bf103185e7f900d3c4a8
elboga5 <59121972+elboga5@users.noreply.github.com>
2024-03-22 13:37:49 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,73 @@
+<img src="https://oriondesign.art.br/wp-content/uploads/2023/08/Thumb-Typebot-copiar-4.png">
+
+# Auto Instalador de Typebot
+
+Este autoinstalador se probó utilizando un VPS Contabo vacío (4vCores + 8Gb Ram) con **Ubunto 20.04**
+En esta versión, puedes elegir los puertos durante la instalación, lo que permite integrar Typebot con cualquier otra aplicación.
+
+### 📌 Notas importantes:
+
+Instalación realizada en Docker (si tiene otros contenedores, pueden fallar durante el proceso de instalación con este instalador automático;
+
+Recomiendo crear una instantánea de su VPS para evitar cualquier problema que pueda ocurrir;
+
+Antes de la instalación, es extremadamente importante que cree 3 registros en el DNS, a saber, “typebot”, “bot” y “storage”. Todos de Tipo **A**, todos apuntando a la **ip de tu VPS**, con **Proxy desactivado** y **TTL Auto** (o el valor que venga).
+<details>
+   <summary>Acerca del registro de Typebot</summary>
+   <i>Es a través de este que podremos acceder al sistema para crear nuestro bot, también conocido como Builder</i><br><br>
+   • Tipo: <b>A</b><br>
+   • Entrada: <b>typebot</b><br>
+   • Contenido: <b>IP del servidor</b><br><br>
+   <img src="https://file.notion.so/f/s/c14b5ac1-d43a-4f18-bd76-4f10bd4262f1/Untitled.png?id=9855df72-743c-439d-b865-ec8391b93cc4&table=block&spaceId=f554c1aa-b56c -4ac0-88b1-4679371e6777&expirationTimestamp=1692072000000&signature=whfO8e8AETlGp2JEWdt0ML-i1QIlPr4kejWSGPXk-qY&downloadName=Untitled.png">
+</details>
+<details>
+   <summary>Acerca del registro de bot</summary>
+   <i>Esta es la pantalla Bot Viewer, por lo que cuando publiques tu bot, aparecerá a través del enlace bot.tudominio.com.br</i><br><br>
+   • Tipo: <b>A</b><br>
+   • Entrada: <b>bot</b><br>
+   • Contenido: <b>IP del servidor</b><br><br>
+   <img src="https://file.notion.so/f/s/236f6cc3-4857-4c48-a9d0-8b0b35c0ba94/Untitled.png?id=5703d967-1b89-423f-a6b1-60a377785be4&table=block&spaceId=f554c1aa-b56c -4ac0-88b1-4679371e6777&expirationTimestamp=1692072000000&signature=k1X9OIvmNeNfFzrKQg5xpqcS-HLcY9_x4zoc1sq8M6o&downloadName=Untitled.png">
+</details>
+<details>
+   <summary>Acerca del registro de almacenamiento</summary>
+   <i>Lo usamos para guardar imágenes, videos y audios en nuestro servidor, por lo que no necesita exportar su contenido a ningún otro lugar y usar el enlace en nuestro Typebot.</i><br><br>
+   • Tipo: <b>A</b><br>
+   • Entrada: <b>almacenamiento</b><br>
+   • Contenido: <b>IP del servidor</b><br><br>
+   <img src="https://file.notion.so/f/s/571842de-ad54-42e3-980e-542204b6ad0c/Untitled.png?id=c5772588-3c3f-4bd9-ad5c-8c7fc29d3d0a&table=block&spaceId=f554c1aa-b56c-4ac0-88b1-4679371e6777&expirationTimestamp=1692072000000&signature=OHFt_mSTmTRB9PEnhwllhnHQdCbzOa69ewqj5_PYRIc&downloadName=Untitled.png">
+</details>
+
+<hr/>
+
+## 📀 Instalación de Typebot
+
+1- Copia el siguiente comando y pégalo en tu terminal:
+```
+sudo apt install -y git && git clone https://github.com/elboga5/typebot.git && cd typebot && chmod +x typebot.sh && ./typebot.sh
+```
+
+Cuando ejecute este código, le pedirá que proporcione la siguiente información:
+   - Enlace del constructor (por ejemplo, typebot.tudominio.com);
+   - Puerto del constructor (predeterminado: 3001);
+   - Enlace del visor (por ejemplo, bot.tudominio.com);
+   - Puerto del visor (predeterminado: 3002);
+   - Enlace de almacenamiento (por ejemplo, almacenamiento.sudominio.com);
+   - Puerto de almacenamiento (predeterminado: 9000);
+   - Su correo electrónico (por ejemplo, contacto@dominio.com);
+   - Contraseña de su correo electrónico (si es gmail, debe ser la contraseña de la aplicación);
+   - SMTP desde su correo electrónico (ej: smtp.hostinger.com);
+   - Puerto SMTP (ej: 465);
+   - SMTP_SECURE (si el puerto SMTP es 465, escriba verdadero; de lo contrario, escriba falso);
+   - Clave secreta de 32 caracteres (generada en el sitio web: <a href="https://codebeautify.org/generate-random-hexadecimal-numbers">codebeautify</a>).
+
+Después de completar los campos correctamente, comenzará a instalar y configurar Typebot.
+
+>
+> Al final de la instalación aparecerá un mensaje, escriba "Y" y "ENTER" para completar la instalación de typebot.
+>
+
+<hr/>
+
+<br><br><br>
+Ahora quédate con el pato bailarín:<br><br>
+<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3hpaTI2dzVuMGZmMnFteWE1bW80Z29hYXZub3cybTQyZHFrc2VoaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/b9QBHfcNpvqDK/giphy.gif">
---
+++ typebot.sh
@@ -0,0 +1,355 @@
+#######################################################
+
+clear
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m _                             _              _               \e[0m"
+echo -e "\e[32m| |                _          | |            | |              \e[0m"
+echo -e "\e[32m| | ____    ___  _| |_  _____ | |  _____   __| |  ___    ____ \e[0m"
+echo -e "\e[32m| ||  _ \  /___)(_   _)(____ || | (____ | / _  | / _ \  / ___)\e[0m"
+echo -e "\e[32m| || | | ||___ |  | |_ / ___ || | / ___ |( (_| || |_| || |    \e[0m"
+echo -e "\e[32m|_||_| |_|(___/    \__)\_____| \_)\_____| \____| \___/ |_|    \e[0m"
+echo -e "\e[32m                                                              \e[0m"
+echo -e "\e[32m _______                       _                   \e[0m"
+echo -e "\e[32m(_______)                     | |              _   \e[0m"
+echo -e "\e[32m    _     _   _  ____   _____ | |__    ___   _| |_ \e[0m"
+echo -e "\e[32m   | |   | | | ||  _ \ | ___ ||  _ \  / _ \ (_   _)\e[0m"
+echo -e "\e[32m   | |   | |_| || |_| || ____|| |_) )| |_| |  | |_ \e[0m"
+echo -e "\e[32m   |_|    \__  ||  __/ |_____)|____/  \___/    \__)\e[0m"
+echo -e "\e[32m         (____/ |_|                                \e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+
+sleep 3
+#######################################################
+
+read -p "Url para builder (ej: typebot.sudominio.com): " builder
+echo ""
+read -p "Puerto de Builder (predeterminado: 3001): " portabuilder #novo
+echo ""
+read -p "Url para Viewer (ej: bot.sudominio.com): " viewer
+echo ""
+read -p "Puerto de Viewer (predeterminado: 3002): " portaviewer #novo
+echo ""
+read -p "Url para Storage (ej: storage.sudominio.com): " storage
+echo ""
+read -p "Puerto de Storage (predeterminado: 9000): " portastorage #novo
+echo ""
+read -p "Su Email (ex: contacto@dominio.com): " email
+echo ""
+read -p "Contraseña para tu correo electrónico (si es gmail, debe ser la contraseña de la aplicación): " senha
+echo ""
+read -p "SMTP de su email (ej: smtp.gmail.com): " smtp
+echo ""
+read -p "Puerto SMTP (ej: 465): " portasmtp #novo
+echo ""
+read -p "SMTP_SECURE (Si el puerto SMTP es 465, escriba true; de lo contrario, escriba false): " SECURE #novo
+echo ""
+read -p "Chave secreta de 32 caracteres: (ex: c56f3775313440c3edce57529a0f02b4): " key
+
+#######################################################
+
+echo ""
+echo ""
+echo ""
+
+echo "Actualizando sistema e Instalando Docker Compose + Nginx + Certbot"
+
+sleep 3
+
+cd
+cd
+
+clear
+
+sudo apt update -y
+sudo apt upgrade -y
+
+apt install docker-compose -y
+sudo apt update
+
+sudo apt install nginx -y
+sudo apt update
+
+sudo apt install certbot -y
+sudo apt update
+
+mkdir typebot.io
+cd typebot.io
+echo ""
+echo ""
+echo "Actualizado/Instalado con Exito"
+
+sleep 3
+
+clear
+
+#######################################################
+
+echo "Creando archivo docker-compose.yml"
+
+sleep 3
+
+cat > docker-compose.yml << EOL
+version: '3.3'
+services:
+  typebot-db:
+    image: postgres:13
+    restart: always
+    volumes:
+      - db_data:/var/lib/postgresql/data
+    environment:
+      - POSTGRES_DB=typebot
+      - POSTGRES_PASSWORD=typebot
+  typebot-builder:
+    ports:
+      - $portabuilder:3000
+    image: baptistearno/typebot-builder:latest
+    restart: always
+    depends_on:
+      - typebot-db
+    environment:
+      - DATABASE_URL=postgresql://postgres:typebot@typebot-db:5432/typebot
+      - NEXTAUTH_URL=https://$builder
+      - NEXT_PUBLIC_VIEWER_URL=https://$viewer
+
+      - ENCRYPTION_SECRET=$key
+
+      - ADMIN_EMAIL=$email
+
+      - SMTP_SECURE=$SECURE
+
+      - SMTP_HOST=$smtp
+      - SMTP_PORT=$portasmtp
+      - SMTP_USERNAME=$email
+      - SMTP_PASSWORD=$senha
+      - NEXT_PUBLIC_SMTP_FROM='Suporte Typebot' <$email>
+
+      - S3_ACCESS_KEY=minio
+      - S3_SECRET_KEY=minio123
+      - S3_BUCKET=typebot
+      - S3_ENDPOINT=$storage
+  typebot-viewer:
+    ports:
+      - $portaviewer:3000
+    image: baptistearno/typebot-viewer:latest
+    restart: always
+    environment:
+      - DATABASE_URL=postgresql://postgres:typebot@typebot-db:5432/typebot
+      - NEXT_PUBLIC_VIEWER_URL=https://$viewer
+      - ENCRYPTION_SECRET=$key
+
+      - S3_ACCESS_KEY=minio
+      - S3_SECRET_KEY=minio123
+      - S3_BUCKET=typebot
+      - S3_ENDPOINT=$storage
+  mail:
+    image: bytemark/smtp
+    restart: always
+  minio:
+    labels:
+      virtual.host: '$storage'
+      virtual.port: '$portastorage'
+      virtual.tls-email: '$email'
+    image: minio/minio
+    command: server /data
+    ports:
+      - '$portastorage:$portastorage'
+    environment:
+      MINIO_ROOT_USER: minio
+      MINIO_ROOT_PASSWORD: minio123
+    volumes:
+      - s3_data:/data
+  createbuckets:
+    image: minio/mc
+    depends_on:
+      - minio
+    entrypoint: >
+      /bin/sh -c "
+      sleep 10;
+      /usr/bin/mc config host add minio http://minio:$portastorage minio minio123;
+      /usr/bin/mc mb minio/typebot;
+      /usr/bin/mc anonymous set public minio/typebot/public;
+      exit 0;
+      "
+volumes:
+  db_data:
+  s3_data:
+EOL
+
+echo "Creado y configurado exitosamente"
+
+sleep 3
+
+clear
+
+###############################################
+
+echo "Iniciando Contenedor"
+
+sleep 3
+
+docker-compose up -d
+
+echo "Typebot Instaldo... Realizando Proxy Reverso"
+
+sleep 3
+
+clear
+
+###############################################
+
+cd
+
+cat > typebot << EOL
+server {
+
+  server_name $builder;
+
+  location / {
+
+    proxy_pass http://127.0.0.1:$portabuilder;
+
+    proxy_http_version 1.1;
+
+    proxy_set_header Upgrade \$http_upgrade;
+
+    proxy_set_header Connection 'upgrade';
+
+    proxy_set_header Host \$host;
+
+    proxy_set_header X-Real-IP \$remote_addr;
+
+    proxy_set_header X-Forwarded-Proto \$scheme;
+
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    
+    proxy_cache_bypass \$http_upgrade;
+
+	  }
+
+  }
+EOL
+
+###############################################
+
+sudo mv typebot /etc/nginx/sites-available/
+
+sudo ln -s /etc/nginx/sites-available/typebot /etc/nginx/sites-enabled
+
+###############################################
+
+cd
+
+cat > viewer << EOL
+server {
+
+  server_name $viewer;
+
+  location / {
+
+    proxy_pass http://127.0.0.1:$portaviewer;
+
+    proxy_http_version 1.1;
+
+    proxy_set_header Upgrade \$http_upgrade;
+
+    proxy_set_header Connection 'upgrade';
+
+    proxy_set_header Host \$host;
+
+    proxy_set_header X-Real-IP \$remote_addr;
+
+    proxy_set_header X-Forwarded-Proto \$scheme;
+
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    
+    proxy_cache_bypass \$http_upgrade;
+
+	  }
+
+  }
+EOL
+
+###############################################
+
+sudo mv viewer /etc/nginx/sites-available/
+
+sudo ln -s /etc/nginx/sites-available/viewer /etc/nginx/sites-enabled
+
+##################################################
+
+cd
+
+cat > storage << EOL
+server {
+
+  server_name $storage;
+
+  location / {
+
+    proxy_pass http://127.0.0.1:$portastorage;
+
+    proxy_http_version 1.1;
+
+    proxy_set_header Upgrade \$http_upgrade;
+
+    proxy_set_header Connection 'upgrade';
+
+    proxy_set_header Host \$host;
+
+    proxy_set_header X-Real-IP \$remote_addr;
+
+    proxy_set_header X-Forwarded-Proto \$scheme;
+
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    
+    proxy_cache_bypass \$http_upgrade;
+
+	  }
+
+  }
+EOL
+
+###############################################
+
+sudo mv storage /etc/nginx/sites-available/
+
+sudo ln -s /etc/nginx/sites-available/storage /etc/nginx/sites-enabled
+
+###############################################
+
+sudo certbot -m $email \
+          --nginx \
+          --agree-tos \
+          --non-interactive \
+          --domains $builder,$viewer,$storage
+
+
+###############################################
+
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m _                             _              _        \e[0m"
+echo -e "\e[32m| |                _          | |            | |       \e[0m"
+echo -e "\e[32m| | ____    ___  _| |_  _____ | |  _____   __| |  ___  \e[0m"
+echo -e "\e[32m| ||  _ \  /___)(_   _)(____ || | (____ | / _  | / _ \ \e[0m"
+echo -e "\e[32m| || | | ||___ |  | |_ / ___ || | / ___ |( (_| || |_| |\e[0m"
+echo -e "\e[32m|_||_| |_|(___/    \__)\_____| \_)\_____| \____| \___/ \e[0m"
+echo -e "\e[32m                                                       \e[0m"              
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m Accede a Typebot a través del enlace: https://$builder\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m MCX SAS - soluciones en internet - https:mcx.com.co\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
+echo -e "\e[32m\e[0m"
